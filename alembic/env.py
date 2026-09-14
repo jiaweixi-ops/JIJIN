86e46c2e875da3fbce561467b0c9966a4df47799
jiaweixi-ops <jiaweixi520@gmail.com>
2026-09-14 11:22:57 +0800

    db: register qualification model metadata

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,7 @@ from app import (  # noqa: F401
     hardening_models,
     models,
     operational_models,
+    qualification_models,
     research_models,
     review_models,
     security_models,
